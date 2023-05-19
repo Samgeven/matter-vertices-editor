3db34f9348e86ad6eb8fafe57f61abc9da6d591f
Samgeven <sam.geven@mail.ru>
2023-05-19 16:49:03 +0300

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_arff.69e82ae4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkvertices_editor=self.webpackChunkvertices_editor||[]).push([[1438],{7209:function(e){function t(e){e.languages.arff={comment:/%.*/,string:{pattern:/(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,greedy:!0},keyword:/@(?:attribute|data|end|relation)\b/i,number:/\b\d+(?:\.\d+)?\b/,punctuation:/[{},]/}}e.exports=t,t.displayName="arff",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_arff.69e82ae4.chunk.js.map
---
+++ static/js/react-syntax-highlighter_languages_refractor_git.52275966.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkvertices_editor=self.webpackChunkvertices_editor||[]).push([[2564],{5120:function(e){function t(e){e.languages.git={comment:/^#.*/m,deleted:/^[-\u2013].*/m,inserted:/^\+.*/m,string:/("|')(?:\\.|(?!\1)[^\\\r\n])*\1/m,command:{pattern:/^.*\$ git .*$/m,inside:{parameter:/\s--?\w+/m}},coord:/^@@.*@@$/m,commit_sha1:/^commit \w{40}$/m}}e.exports=t,t.displayName="git",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_git.52275966.chunk.js.map
